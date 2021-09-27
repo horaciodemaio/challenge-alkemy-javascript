75a3b9dcae36a0310070dfef22d6581bcfb986c8
horaciodemaio <horaciodemaio33@gmail.com>
2021-09-27 19:27:07 -0300

    api/ correccion de nombre de carpeta register

--- api/index.js
+++ api/index.js
@@ -1,31 +1,10 @@
-// const {Diet} = require("./src/db");
-// const axios = require("axios");
-// const {ArrayDietas} = require("./contents")
-const {
-  API_KEY
-} = process.env;
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-// const {v4: UUIDV4} = require("uuid");
 
-// Syncing all the models at once.
 conn.sync({ force: true })
 .then(() => {
-  // console.log("sincroniza");
   
-  // const Dietas = ArrayDietas.map((d) => {
-  //   return {
-  //     name: d.name,
-  //   }
-  // });
-  // Diet.bulkCreate(Dietas).then(console.log("me cargaron los datos en la tabla de dietas"))
-  
-  
-  // const DietDB = await Diet.findOrCreate({
-  //   where:{
-      
-  //   }
-  // })
 
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
--- api/src/routes/allRegister.js
+++ api/src/routes/allRegister.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const {getAllRegister} = require("../controlls/register")
+const {getAllRegister} = require("../controlls/GetAllRegister")
 const router = Router();
 
 router.get("/", getAllRegister);
