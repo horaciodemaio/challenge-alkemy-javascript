e20adfa4e0a422b6a5ea4a927af67ee8227f2c8a
horaciodemaio <horaciodemaio33@gmail.com>
2021-09-27 17:15:02 -0300

    armado de tabla de tipo ingreso o egreso

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -3,6 +3,9 @@ const { DataTypes, UUIDV4 } = require('sequelize');
 module.exports = (sequelize) => {
 
   sequelize.define('Type', {
-   
+   type:{
+    type: DataTypes.ENUM(["ingress","egress"]),
+    allowNull:false,
+   }
   });
 };
