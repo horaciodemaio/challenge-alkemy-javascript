87334350802c055b15dd92fe54b401a0cc7e3020
horaciodemaio <horaciodemaio33@gmail.com>
2021-09-28 20:09:21 -0300

    api/ filtro de tipo ingreso o egreso listo

--- api/src/controlls/GetAllRegister.js
+++ api/src/controlls/GetAllRegister.js
@@ -2,16 +2,38 @@ const { Register, Type } = require('../db');
 
 
 async function getAllRegister(req, res, next) {
+    var type = req.query.type;
     try {
-        var allRegister = await Register.findAll({
-            attributes: { exclude: ["createdAt", "updatedAt"] },
-            include: [
-                { model: Type, attributes: { exclude: ["createdAt", "updatedAt"] } }
-            ]
-        }
-        );
+        if (type) {
 
-        res.json(allRegister);
+            var findOne = await Register.findAll({
+                // limit: limit,
+                // offset: offset,
+                include: {
+                    model: Type,
+                    attributes: ["name"],
+                    where: {
+                        name: type,
+                    },
+                },
+            });
+
+            if (findOne.length === 0) {
+                return res.status(404).send("Error: Name of type is invalid");
+            } else return res.json(findOne);
+
+        } else {
+
+            var allRegister = await Register.findAll({
+                attributes: { exclude: ["createdAt", "updatedAt"] },
+                include: [
+                    { model: Type, attributes: { exclude: ["createdAt", "updatedAt"] } }
+                ]
+            }
+            );
+
+            res.json(allRegister);
+        }
 
     } catch (error) {
 
